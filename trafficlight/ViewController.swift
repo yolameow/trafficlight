4c6c1aa3b8aa89d89f8e9e39659b06a8292c1bf8
Olga Yushkevich <olyal-ya@mail.ru>
2020-10-15 02:10:35 +0300

    y

--- trafficlight/ViewController.swift
+++ trafficlight/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startOrNextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        
+        redLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
     }
 
-
+    @IBAction func pressedButton() {
+        if  startOrNextButton.isEnabled {
+            startOrNextButton.setTitle("NEXT", for: .normal)
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+        }
+        /* startOrNextButton.setTitle("NEXT", for: .selected)
+         if startOrNextButton.isSelected {
+            startOrNextButton.title(for: .selected)
+        }
+            
+           redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            redLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            redLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        } */
+        
+    }
+    
 }
 
