a78ea219db91a149aa51b34ff82f559e1f427428
Olga Yushkevich <olyal-ya@mail.ru>
2020-10-14 23:27:22 +0300

    Initial Commit

---
+++ trafficlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficlight
+//
+//  Created by Mac on 14.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
